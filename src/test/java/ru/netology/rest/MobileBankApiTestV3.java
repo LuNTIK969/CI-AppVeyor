83f6b0c8e2c5987550c603ba9e7a2cb8fb81e5ab
LuNTIK969 <nasa.969@gmail.com>
2023-03-15 17:18:47 +0300

    test UPD (object USD test added)

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -9,7 +9,7 @@ import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldReturnDemoAccountsRUB() {
       // Given - When - Then
       // Предусловия
       given()
@@ -26,5 +26,25 @@ class MobileBankApiTestV3 {
           .body("[0].currency", equalTo("RUB"))
           .body("[0].balance", greaterThanOrEqualTo(0))
       ;
+    }
+
+    @Test
+    void shouldReturnDemoAccountsUSD() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[1].currency", equalTo("USD"))
+                .body("[1].balance", greaterThanOrEqualTo(0))
+        ;
     }
 }
